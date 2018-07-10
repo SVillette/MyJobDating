b7808cc1a6dc0ef5667139d4d0f3831caba4bd2d
Jessy Bellanger <jessy.bellanger@protonmail.com>
2018-07-10 11:28:03 +0200

    Temporarily removed tests of OfferControllerTest

--- tests/MyJobDatingOfferBundle/Controller/OfferControllerTest.php
+++ tests/MyJobDatingOfferBundle/Controller/OfferControllerTest.php
@@ -6,10 +6,5 @@ use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 
 class OfferControllerTest extends WebTestCase
 {
-    public function testIndex()
-    {
-        $client = static::createClient();
 
-        $crawler = $client->request('GET', '/');
-    }
 }
