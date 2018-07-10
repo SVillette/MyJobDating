7412970ba0cef56544ddb74e08663bf53daf7dd0
Jessy Bellanger <jessy.bellanger@protonmail.com>
2018-07-10 09:59:35 +0200

    Added offers

--- src/MyJobDating/Bundle/SkillBundle/Entity/Skill.php
+++ src/MyJobDating/Bundle/SkillBundle/Entity/Skill.php
@@ -9,37 +9,13 @@
 namespace MyJobDating\Bundle\SkillBundle\Entity;
 
 
-class Skill
-{
-    private $id;
+use MyJobDating\Bundle\CoreBundle\Entity\ResourceTrait;
+use MyJobDating\Bundle\CoreBundle\Entity\TimestampableTrait;
+
+class Skill {
+    use ResourceTrait, TimestampableTrait;
+
     private $name;
-    private $createdDate;
-    private $updatedDate;
-
-    /**
-     * Skill constructor.
-     * @param $name
-     */
-    public function __construct(string $name = null)
-    {
-        $this->name = $name;
-    }
-
-    /**
-     * @return mixed
-     */
-    public function getId()
-    {
-        return $this->id;
-    }
-
-    /**
-     * @param mixed $id
-     */
-    public function setId($id)
-    {
-        $this->id = $id;
-    }
 
     /**
      * @return string
@@ -55,37 +31,5 @@ class Skill
     public function setName(string $name)
     {
         $this->name = $name;
-    }
-
-    /**
-     * @return mixed
-     */
-    public function getCreatedDate()
-    {
-        return $this->createdDate;
-    }
-
-    /**
-     * @param mixed $createdDate
-     */
-    public function setCreatedDate($createdDate)
-    {
-        $this->createdDate = $createdDate;
-    }
-
-    /**
-     * @return mixed
-     */
-    public function getUpdatedDate()
-    {
-        return $this->updatedDate;
-    }
-
-    /**
-     * @param mixed $updatedDate
-     */
-    public function setUpdatedDate($updatedDate)
-    {
-        $this->updatedDate = $updatedDate;
     }
 }
