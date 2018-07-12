62339a9f986ea81cd1caf57fe65fc9c54888a7f7
xRedSwan <Martin.gillet1996@gmail.com>
2018-07-12 15:50:15 +0200

    addes repository

---
+++ src/MyJobDating/Bundle/SkillBundle/Repository/LevelRepository.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace MyJobDating\Bundle\SkillBundle\Repository;
+
+use Doctrine\ORM\EntityRepository;
+
+class LevelRepository extends EntityRepository implements LevelRepositoryInterface
+{
+
+}
---
+++ src/MyJobDating/Bundle/SkillBundle/Repository/LevelRepositoryInterface.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace MyJobDating\Bundle\SkillBundle\Repository;
+
+use Doctrine\Common\Persistence\ObjectRepository;
+
+interface LevelRepositoryInterface extends ObjectRepository
+{
+
+}
